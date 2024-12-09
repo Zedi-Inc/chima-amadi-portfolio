917c5a78fb9276dea963fa03f1841791d9c01413
Kingsley <kingsleyakwa@gmail.com>
2024-12-09 12:22:23 +0100

    first commit

---
+++ src/config/footer.js
@@ -0,0 +1,80 @@
+export const socials = [
+  {
+    name: 'Facebook',
+    url: 'https://facebook.com/chima-amadi',
+    icon: "mdi:twitter",
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/chima-amadi',
+    icon: "mdi:twitter",
+  },
+  {
+    name: 'Youtube',
+    url: 'https://youtube.com/chima-amadi',
+    icon: "mdi:youtube",
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https:/linkedin.com/chima-amadi',
+    icon: "ic:baseline-discord",
+  },
+]
+
+export const footerLists = [
+	{
+		title: 'Landing Pages',
+		items: [
+			{
+				title: 'Landing Page 1',
+				slug: '/landing-pages/landing-1',
+			},
+			{
+				title: 'Landing Page 2',
+				slug: '/landing-pages/landing-2',
+			},
+      // {
+			// 	title: 'Landing Page 3',
+			// 	slug: '/landing-pages/landing-3',
+			// },
+		],
+	},
+	{
+		title: 'Company',
+		items: [
+			{
+				title: 'About',
+				slug: '/company/about',
+			},
+			{
+				title: 'Blog',
+				slug: '/blog',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	},
+	{
+		title: 'Theme',
+		items: [
+			{
+				title: 'Get Started',
+				slug: '/theme/get-started',
+			},
+			{
+				title: 'Style Guide',
+				slug: '/theme/style-guide',
+			},
+      {
+        title: 'Theme Setup',
+        slug: '/theme/theme-setup',
+      },
+      {
+        title: 'Customizing Odyssey',
+        slug: '/theme/customizing-odyssey',
+      }
+		],
+	},
+];
---
+++ src/config/nav.js
@@ -0,0 +1,18 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About',
+		slug: '/about',
+	},
+	{
+		title: 'Articles',
+		slug: '/blog',
+	},
+	{
+		title: 'Reference',
+		slug: '/reference',
+	},
+];
